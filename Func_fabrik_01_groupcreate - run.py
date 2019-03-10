6a37719f994fe4807387d552b0b9a851a2f52f8e
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-03-10 06:15:56 +0200

    Update Func_fabrik_01_groupcreate - run.py
    Comments

--- Func_fabrik_01_groupcreate - run.py
+++ Func_fabrik_01_groupcreate - run.py
@@ -1,27 +1,32 @@
+""" ****************************************************************************
+Script to RUN CREATE Joomla Fabrik group record
+Copyright (C) AB Janse van Rensburg 20190308
+*****************************************************************************"""
 
+# IMPORT SYSTEM OBJECTS
 import sys
 
-# Add own module path
+# ADD OWN MODULE PATH
 sys.path.append('S:/_my_modules')
-sys.path.append('S:/')
 
+# IMPORT FUNCTIONS
 import funcfile
-import funcmail
 import funcsys
 
+# OPEN THE LOG WRITER
 funcfile.writelog("Now")
 funcfile.writelog("SCRIPT: FUNC_FABRIK_01_GROUPCREATE_RUN")
 funcfile.writelog("--------------------------------------")
 
+# CALL THE FUNCTION
 import Func_fabrik_01_groupcreate
-
 try:
-    i_gr = Func_fabrik_01_groupcreate.Fabrik_groupcreate()
+    i_gr = Func_fabrik_01_groupcreate.Fabrik_groupcreate("T")
     print(i_gr)
 except Exception as e:
     funcsys.ErrMessage(e)
 
-# Close the log writer
+# CLOSE THE LOG WRITER
 funcfile.writelog("Now")
 funcfile.writelog("COMPLETED: FUNC_FABRIK_01_GROUPCREATE_RUN")
 funcfile.writelog("-----------------------------------------")
